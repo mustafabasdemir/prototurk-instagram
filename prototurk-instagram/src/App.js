256db6692717fc4d7d20ac05ad23cc8e3212dbf4
Mustafa Başdemir <78982677+mustafabasdemir@users.noreply.github.com>
2024-10-29 00:18:23 +0300

    login ekranı telefon eklendi

--- prototurk-instagram/src/App.js
+++ prototurk-instagram/src/App.js
@@ -3,7 +3,13 @@ function App() {
   return (
    <>
    <div className="h-full w-full flex items-center justify-center">
-      İnstagram Clone!
+    {/* login phone / relative: sayfada yerini korur*/}
+    <div className="w-[380px] h-[581px] relative bg-logo-pattern bg-[length:468.32px_634.15px] bg-[top_left_-46px]">
+      {/* phone picture */}
+      <div className="w-[250px] h-[538px] absolute top-[27px] right-[18px]">
+        <img className="w-full h-full" src="https://static.cdninstagram.com/images/instagram/xig/homepage/screenshots/screenshot3-2x.png?__d=www" alt=""/>
+      </div>
+    </div>
     </div>
    </>
   );
--- prototurk-instagram/tailwind.config.js
+++ prototurk-instagram/tailwind.config.js
@@ -4,7 +4,13 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      //ınstagram login phone ımage
+      backgroundImage:{
+        'logo-pattern':'url(https://static.cdninstagram.com/images/instagram/xig/homepage/phones/home-phones-2x.png?__makehaste_cache_breaker=73SVAexZgBW)'
+      }
+
+    },
   },
   plugins: [],
 }

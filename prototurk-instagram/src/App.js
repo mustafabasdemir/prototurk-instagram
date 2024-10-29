2cc4cb72b83ca6ffc1f611150eb3452305e25cf3
Mustafa Başdemir <78982677+mustafabasdemir@users.noreply.github.com>
2024-10-29 08:37:48 +0300

    düzenleme yapıldı

--- prototurk-instagram/src/App.js
+++ prototurk-instagram/src/App.js
@@ -7,7 +7,8 @@ function App() {
     <div className="w-[380px] h-[581px] relative bg-logo-pattern bg-[length:468.32px_634.15px] bg-[top_left_-46px]">
       {/* phone picture */}
       <div className="w-[250px] h-[538px] absolute top-[27px] right-[18px]">
-        <img className="w-full h-full" src="https://static.cdninstagram.com/images/instagram/xig/homepage/screenshots/screenshot3-2x.png?__d=www" alt=""/>
+      <img className="w-full h-full" src="https://static.cdninstagram.com/images/instagram/xig/homepage/screenshots/screenshot3-2x.png?__d=www" alt=""/>
+      <img className="w-full h-full" src="https://static.cdninstagram.com/images/instagram/xig/homepage/screenshots/screenshot1-2x.png?__d=www" alt=""/>
       </div>
     </div>
     </div>
